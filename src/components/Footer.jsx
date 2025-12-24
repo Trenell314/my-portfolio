90437b23fdaabef7d63b248ae98561a9d86ceeb8
Trenell <142370039+Trenell314@users.noreply.github.com>
2025-12-24 16:16:19 -0500

    Added Footer Parallax Effect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import {motion} from 'framer-motion';
+import {useRef} from 'react';
+import {motion, useScroll, useTransform} from 'framer-motion';
 
 const containerVariants = {
   initial: { opacity: 0 },
@@ -20,7 +21,7 @@ const childVariants = {
     y: 0,
     transition: {
       type: 'spring',
-      stiffness: 100
+      stiffness: 50
     }
   }
 };
@@ -36,20 +37,21 @@ const starVariants = {
 };
 
 const Footer = () => {
+  const ref = useRef(null);
+  const {scrollYProgress} = useScroll({
+    target: ref,
+    offset: ["start end", "end end"],
+  });
+
+  const textY = useTransform(scrollYProgress, [0, 1], ["-20vh", "0vh"]);
   return (
-    <footer>
+
+    <footer ref={ref}>
       <div className="footer" id="contact-section">
         <div className="footer-wrap">
           <a href="#hero-section">
           <motion.img src="/images/Trenell_SS_Logo.svg" alt="Trenell Holmes Logo" className="footer-logo" 
-          variants={containerVariants}
-          initial={{opacity: 0,
-          y: 100}}
-          whileInView={{opacity: 1,
-          y: 0,
-          transition: {
-          type: 'spring',
-          stiffness: 100}}}/>
+           style={{y: textY}}/>
           </a>
           <motion.div className="footer-links" 
           variants={containerVariants}
@@ -58,24 +60,24 @@ const Footer = () => {
             <a href="https://www.linkedin.com/in/trenell-holmes-9b9a72274/" target="_blank">
             <motion.img src="/images/linkedin_icon.svg" alt="LinkedIn Icon" className="footer-icon" variants={childVariants}
              whileHover={{
-             scale: 1.2}}/>
+             scale: 0.9}}/>
             </a>
 
             <a href="mailto:trenell314@gmail.com">
             <motion.img src="/images/email_icon.svg" alt="Email Icon" className="footer-icon" variants={childVariants}
              whileHover={{
-             scale: 1.2}}/>
+             scale: 0.9}}/>
             </a>
 
             <a href="https://github.com/Trenell314" target="_blank">
             <motion.img src="/images/github_icon.svg" alt="Github Icon" className="footer-icon" variants={childVariants}
              whileHover={{
-             scale: 1.2}}/>
+             scale: 0.9}}/>
             </a>
           </motion.div>
           <div className="footer-stars">
             <motion.img src="/images/star.svg" alt="a star svg" 
-             animate={{rotate: 360}}
+             animate={{rotate: -360}}
              transition={{ease: "linear", duration: 3, repeat: Infinity}}/>
             <motion.img src="/images/star.svg" alt="a star svg" 
              animate={{rotate: 360}}
